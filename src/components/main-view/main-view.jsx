ef708e91c75eb17fe6e9964300673da7f2218874
BriWins <95152254+BriWins@users.noreply.github.com>
2022-06-13 13:58:22 -0500

    building app

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class MainView extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            movies: []
+        }
+    }
+
+    render(){
+        const { movies } = this.state;
+
+        if (movies.length ===0) return <div className="main-view">The list is empty!</div>;
+
+        return (
+            <div className="main-view">
+                {movies.map(movie => <div key={movie._id}>{movie.Title}</div>)}
+            </div>
+        );
+    }
+}
+
+export default MainView;
