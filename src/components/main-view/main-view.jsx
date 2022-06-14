35bf20d5d185850cd9d4e443fab0fd3ba3013c79
BriWins <95152254+BriWins@users.noreply.github.com>
2022-06-14 10:53:51 -0500

    updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import MovieCard from "../movie-card/movie-card";
 import MovieView from "../movie-view/movie-view";
 
@@ -12,6 +14,18 @@ class MainView extends React.Component {
         };
     }
 
+    componentDidMount(){
+        axios.get(`https://glacial-shore-06302.herokuapp.com/movies`)
+            .then( response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch( error => {
+                console.log(error);
+            });
+    }
+
      setSelectedMovie(newSelectedMovie) {
         this.setState({
             selectedMovie: newSelectedMovie
@@ -22,18 +36,15 @@ class MainView extends React.Component {
     render(){
         const { movies, selectedMovie } = this.state;
 
-    
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
-        if (movies.length ===0) return <div className="main-view">The list is empty!</div>;
-
-      
         return (
             <div className="main-view">
                 {selectedMovie
                 ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                 : movies.map(movie => (
                 <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => {this.setSelectedMovie(movie) }} />
-                ))
+                    ))
                 }
             </div>
         );
