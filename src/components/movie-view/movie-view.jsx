4e1c00b5a49ac9eb49fb3b7a00ae798ee98f871f
BriWins <95152254+BriWins@users.noreply.github.com>
2022-06-14 08:49:17 -0500

    added movie-view

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+class MovieView extends React.Component {
+    render() {
+        const { movieData } = this.props;
+
+        return 
+
+        <div className="movie-view">
+            <div className="movie-poster">
+                <img src={movie.ImgPath} crossorigin="anonymous" />
+            </div>
+            <div className="movie-title">
+                <span  className="label">Title: </span>
+                <span  className="value">{movie.Title}</span>
+            </div>
+            <div className="movie-description">
+                <span  className="label">Description: </span>
+                <span  className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-release">
+                <span className="label">Theater Release Date: </span>
+                <span className="value">{movie.ReleaseDate}</span>
+            </div>
+            <div className="movie-rating">
+                <span className="label">Rating: </span>
+                <span className="value">{movie.Rating}</span>
+            </div>
+            <div className="movie-director">
+                <span className="label">Director: </span>
+                <span className="value">{movie.Director}</span>
+            </div>
+            <div className="movie-genre">
+                <span className="label">Genre: </span>
+                <span className="value">{movie.Genre}</span>
+            </div>
+            <div className="movie-actors">
+                <span className="label">Actors: </span>
+                <span className="value">{movie.Actors}</span>
+            </div>
+            <div className="movie-feature">
+                <span className="label">Featured Movie?: </span>
+                <span className="value">{movie.Featured}</span>
+            </div>
+        </div>
+    }
+}
+
+export default MovieView;
